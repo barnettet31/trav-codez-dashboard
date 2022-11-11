a1a2b89f05bd88568337c679932c46a5b6eb93ff
Travis Barnette <barnette.travis31@gmail.com>
2022-11-11 08:50:55 -0500

    learning trpc to dynamically grab data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,102 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 import { trpc } from "../utils/trpc";
+const Projects = () => {
+  const { data: projects, isLoading } = trpc.projects.getAll.useQuery();
+  if (isLoading) return <div>Loading your messages...</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      {projects?.map((project, index) => {
+        return (
+          <div key={index}>
+            <p>{project.name}</p>
+            <p>{project.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
-  const { status, data: session, } = useSession();
+  const { status, data: session } = useSession();
   if (status === "loading") {
-    return <main><h1>Loading...</h1></main>;
+    return (
+      <main className="flex flex-col items-center pt-4">
+        <h1>Loading...</h1>
+      </main>
+    );
   }
+  const ProjectForm = () => {
+    const [description, setDescription] = useState("");
+    const utils = trpc.useContext();
+    const postProject = trpc.projects.postProject.useMutation({
+      onMutate: () => {
+        utils.projects.getAll.cancel();
+        const optimisticUpdate = utils.projects.getAll.getData();
+        if (optimisticUpdate) {
+          utils.projects.getAll.setData(optimisticUpdate);
+        }
+      },
+      onSettled: () => {
+        utils.projects.getAll.invalidate();
+      },
+    });
+    return (
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          postProject.mutate({
+            name: session?.user?.name as string,
+            description,
+          });
+          setDescription("");
+        }}
+        className="flex gap-2"
+      >
+        <input
+          type="text"
+          value={description}
+          placeholder="your description here"
+          minLength={2}
+          maxLength={200}
+          onChange={(event) => setDescription(event.target.value)}
+          className="rounded-md border-2 border-zinc-800 px-4 py-2 text-black focus:outline-none"
+        ></input>
+        <button
+          type="submit"
+          className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  };
   return (
     <>
-      <main>{
-        session?.user ? (<><h1>{session.user.name}</h1><button onClick={() => signOut()}>Sign Out</button></>) : 
-        <>
-        <h1>Hello Next</h1>
-          <button onClick={() => signIn()}>Sign In</button>
-
-          
+      <main className="flex flex-col items-center">
+        <h1 className="pt4 text-3xl">Projects</h1>
+        <p>This is me proving my projects are mapped out.</p>
+        {session ? (
+          <>
+            <h1>{session.user?.name}</h1>
+            <button onClick={() => signOut()}>Sign Out</button>
+            <div className="pt-6">
+              <ProjectForm />
+            </div>
           </>
-      }
-
+        ) : (
+          <>
+            <h1>Hello Next</h1>
+            <button onClick={() => signIn()}>Sign In</button>
+          </>
+        )}
+        <Projects />
       </main>
     </>
   );
 };
 
 export default Home;
-
--- src/server/trpc/router/project.ts
+++ src/server/trpc/router/project.ts
@@ -2,37 +2,45 @@ import { z } from "zod";
 import { router, protectedProcedure } from "../trpc";
 
 export const projectRouter = router({
-    postMessage: protectedProcedure
-        .input(z.object({
-            name: z.string(),
-            description: z.string()
-        }))
-        .mutation(async ({ ctx, input }) => {
-            try {
-                await ctx.prisma.project.create({
-                    data: {
-                        name: input .name,
-                        description: input.description
-                    }
-                })
-            } catch (e) {
-                console.log(e);
-            }
-        }),
-    getAll: protectedProcedure.query(async ({ctx})=>{
-        try {
-            return await ctx.prisma.project.findMany({
-                select:{
-                    name:true,
-                    description:true
-                },
-                orderBy:{
-                    createdAt:"desc"
-                }
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    })
-
-})
+  postProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session?.user?.id;
+      try {
+        await ctx.prisma.project.create({
+          data: {
+            creator: {
+              connect: {
+                id: userId,
+              },
+            },
+            name: input.name,
+            description: input.description,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }),
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.project.findMany({
+        select: {
+          name: true,
+          description: true,
+          bugs: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }),
+});
