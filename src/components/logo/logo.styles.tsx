d581aa8074fa0e99636e394c63c1be9411132ef8
Travis Barnette <barnette.travis31@gmail.com>
2022-11-22 20:05:47 -0500

    finalized logo component

---
+++ src/components/logo/logo.styles.tsx
@@ -0,0 +1,15 @@
+import { cva, type VariantProps } from "class-variance-authority";
+export type SVGLogoVariantProps = VariantProps<typeof svgLogoVariants>;
+export const svgLogoVariants = cva(
+  ["mx-auto", "w-auto", "text-gray-600", "dark:text-gray-300"],
+  {
+    variants: {
+      intent: {
+        large: ["h-72", "w-72"],
+        medium: ["h-60", "w-60"],
+        small: ["h-40", "w-40"],
+        xsmall: ["h-24", "w-24"],
+      },
+    },
+  }
+);
