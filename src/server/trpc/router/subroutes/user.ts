e7fa4b4dd621451ca3d51ec9350131d6d2bdb843
Travis Barnette <barnette.travis31@gmail.com>
2022-11-22 19:25:21 -0500

    modified /me to include user name

--- src/server/trpc/router/subroutes/user.ts
+++ src/server/trpc/router/subroutes/user.ts
@@ -4,9 +4,9 @@ export const userRouter = router({
   me: protectedProcedure.query(async ({ ctx }) => {
     const userResponse = await ctx.prisma.user.findFirst({
       where: { id: ctx.session.user.id },
-      select: { role: true },
+      select: { role: true, name: true },
     });
 
-    return userResponse?.role;
+    return { role: userResponse?.role, name: userResponse?.name };
   }),
 });
